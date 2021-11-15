05ebbf40cc18314d8bfe84e8f4acec89e32ae3f3
daviddpi <piscopodavid97@gmail.com>
2021-11-15 16:04:25 +0100

    aggiunto admin e post come blade e aggiunto homecontroller

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+->namespace('Admin')
+->prefix("admin.")
+->name('admin')
+->group( function(){
+    Route::get("home", "HomeController@index");
+});
+
+Route::get('/home', 'Guests\HomeController@index')->name('guests.home');
