965568ebbcf918a5e5a63b5abd9fb5480f387ee4
daviddpi <piscopodavid97@gmail.com>
2021-11-16 20:00:03 +0100

    aggiunte sul model le relazioni

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Category extends Model
 {
     protected $fillable = ["name"];
+
+    public function posts(){
+        return $this->hasMany("App\Post");
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Category;
 
 class Post extends Model
 {
     protected $fillable = ["title", "author", "post_date", "post_content", "image_url"];
+
+    public function category(){
+        return $this->belongsTo("App\Category");
+    }
 }
