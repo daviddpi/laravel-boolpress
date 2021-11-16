5474cdcf68253915b978528072794bc211b90717
daviddpi <piscopodavid97@gmail.com>
2021-11-16 18:11:44 +0100

    autorizzazione api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::namespace("Api")
+Route::middleware('auth')
+->namespace("Api")
 ->group(function(){
     Route::resource("posts", PostController::class);
 });
