a136198cd7ce6ecee28eec0a8cb02b9826a6ff14
daviddpi <piscopodavid97@gmail.com>
2021-11-18 21:41:50 +0100

    relazione user e userInfo

--- app/UserInfo.php
+++ app/UserInfo.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class UserInfo extends Model
 {
     protected $fillable = ["address", "country", "telephone"];
+
+    public function userInfo(){
+        return $this->hasOne("App\UserInfo");
+    }
 }
